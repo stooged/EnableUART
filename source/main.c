af530a96b8e35defc7448b6d98f8df0d1baae906
Stooged <stooged@users.noreply.github.com>
2018-03-28 13:13:06 +0800

    Initial

---
+++ source/main.c
@@ -0,0 +1,42 @@
+#include "ps4.h"
+#include "patch.h"
+
+void systemMessage(char* msg) {
+ 	char buffer[512]; 
+ 	sprintf(buffer, "%s\n\n\n\n\n\n\n", msg);
+ 	sceSysUtilSendSystemNotificationWithText(0x81, buffer);
+}
+
+
+int _main(struct thread *td) {
+	initKernel();
+	initLibc();
+	syscall(11,patcher,td);
+	initSysUtil();
+	systemMessage("UART Enabled");
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
